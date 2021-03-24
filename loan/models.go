288d24a041cc7529fad5ab154dc7794d372546a4
ArminGodiz <armingodarzi1380@gmail.com>
2021-03-24 22:22:58 +0430

    Block struct added

--- loan/models.go
+++ loan/models.go
@@ -1,7 +1,22 @@
 package loan
 
+// Loan :
 type Loan struct {
 	Name   string `json:"name"`
 	LoanID string `json:"loanId"`
 	Amount int    `json:"amount"`
 }
+
+//Loans is an array of Loan
+type Loans []Loan
+
+//Block :
+type Block struct {
+	Index             int    `json:"index"`
+	Timestamp         int64  `json:"timestamp"`
+	Loans             Loans  `json:"loans"`
+	Nonce             int    `json:"nonce"`
+	Hash              string `json:"hash"`
+	PreviousBlockHash string `json:"previousBlockHash"`
+}
+
