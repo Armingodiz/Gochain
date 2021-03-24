a4a5df02a97642a26dd344b965631c58f890bccd
ArminGodiz <armingodarzi1380@gmail.com>
2021-03-24 23:27:37 +0430

    controller implemention : Index&GetBlockChain added

--- loan/controller.go
+++ loan/controller.go
@@ -1,8 +1,33 @@
 package loan
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 //Controller :
 type Controller struct {
 	blockchain     *Blockchain
 	currentNodeURL string
 }
+//ResponseToSend ...
+type ResponseToSend struct {
+	Note string
+}
 
+//Index
+func (c *Controller) Index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+}
+
+//GetBlockchain 
+func (c *Controller) GetBlockchain(w http.ResponseWriter, r *http.Request) {
+	data, _ := json.Marshal(c.blockchain)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+	return
+}
