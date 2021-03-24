e3b60bc48949ae4fec978021e3babeb9c9c0bac6
ArminGodiz <armingodarzi1380@gmail.com>
2021-03-25 00:02:10 +0430

    controller implemention :mining part added

--- loan/controller.go
+++ loan/controller.go
@@ -2,10 +2,12 @@ package loan
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 //Controller :
@@ -35,6 +37,7 @@ func (c *Controller) GetBlockchain(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 	return
 }
+
 /////////////////////////////////////////////////////////////////// Making calls for broadcasting part
 //MakeCall :
 func MakeCall(mode string, url string, jsonStr []byte) interface{} {
@@ -74,6 +77,7 @@ func MakeGetCall(url string, jsonStr []byte) interface{} {
 	// call url in GET
 	return MakeCall("GET", url, jsonStr)
 }
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////////// Adding new loan :
@@ -160,3 +164,73 @@ func (c *Controller) RegisterLoan(w http.ResponseWriter, r *http.Request) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////// end loan registration part
+///////////////////////////////////////////////////////////////////////////////////////////// mining part :
+//Mine GET /mine
+func (c *Controller) Mine(w http.ResponseWriter, r *http.Request) {
+	lastBlock := c.blockchain.GetLastBlock()
+	previousBlockHash := lastBlock.Hash
+	currentBlockData := BlockData{Index: strconv.Itoa(lastBlock.Index - 1), Loans: c.blockchain.PendingLoans}
+	currentBlockDataAsByteArray, _ := json.Marshal(currentBlockData)
+	currentBlockDataAsStr := base64.URLEncoding.EncodeToString(currentBlockDataAsByteArray)
+
+	nonce := c.blockchain.ProofOfWork(previousBlockHash, currentBlockDataAsStr)
+	blockHash := c.blockchain.HashBlock(previousBlockHash, currentBlockDataAsStr, nonce)
+	newBlock := c.blockchain.CreateNewBlock(nonce, previousBlockHash, blockHash)
+	blockToBroadcast, _ := json.Marshal(newBlock)
+
+	for _, node := range c.blockchain.NetworkNodes {
+		if node != c.currentNodeURL {
+			// call /receive-new-block in node
+			MakePostCall(node+"/receive-new-block", blockToBroadcast)
+		}
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	var resp ResponseToSend
+	resp.Note = "New block mined and broadcast successfully."
+	data, _ := json.Marshal(resp)
+	w.Write(data)
+	return
+}
+
+//ReceiveNewBlock POST /receive-new-block
+func (c *Controller) ReceiveNewBlock(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body) // read the body of the request
+	if err != nil {
+		log.Fatalln("Error ReceiveNewBlock", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err := r.Body.Close(); err != nil {
+		log.Fatalln("Error ReceiveNewBlock", err)
+	}
+
+	var blockReceived Block
+	if err := json.Unmarshal(body, &blockReceived); err != nil { // unmarshall body contents as a type Candidate
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			log.Fatalln("Error ReceiveNewBlock unmarshalling data", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+
+	var resp ResponseToSend
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	// append block to blockchain
+	if c.blockchain.CheckNewBlockHash(blockReceived) {
+		resp.Note = "New Block received and accepted."
+		c.blockchain.PendingLoans = Loans{}
+		c.blockchain.Chain = append(c.blockchain.Chain, blockReceived)
+	} else {
+		resp.Note = "New Block rejected."
+	}
+
+	data, _ := json.Marshal(resp)
+	w.Write(data)
+	return
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
