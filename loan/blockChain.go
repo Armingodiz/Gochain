f8a208b30eb85e858b1180460aaed95718bb43c7
ArminGodiz <armingodarzi1380@gmail.com>
2021-03-24 22:44:42 +0430

    CreateNewBlock and getLastBlock added to blockchain

--- loan/blockChain.go
+++ loan/blockChain.go
@@ -1,6 +1,9 @@
 package loan
 
-import "strings"
+import (
+	"strings"
+	"time"
+)
 
 //RegisterLoan registers a Loan in our blockchain
 func (b *Blockchain) RegisterBet(loan Loan) bool {
@@ -28,4 +31,20 @@ func contains(s []string, e string) bool {
 	return false
 }
 
+//CreateNewBlock create and add new block to our blockchain (this method will be called after validation)
+func (b *Blockchain) CreateNewBlock(nonce int, previousBlockHash string, hash string) Block {
+	newBlock := Block{
+		Index:     len(b.Chain) + 1,
+		Loans:     b.PendingLoans,
+		Timestamp: time.Now().UnixNano(),
+		Nonce:     nonce,
+		Hash:      hash, PreviousBlockHash: previousBlockHash}
+	b.PendingLoans = Loans{}
+	b.Chain = append(b.Chain, newBlock)
+	return newBlock
+}
 
+//GetLastBlock
+func (b *Blockchain) GetLastBlock() Block {
+	return b.Chain[len(b.Chain)-1]
+}
