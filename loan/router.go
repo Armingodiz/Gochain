c90bcd1c6bbe44927e071137712aab1c77acb2ec
ArminGodiz <armingodarzi1380@gmail.com>
2021-03-24 22:36:26 +0430

    NewRouter function added to router and its implemention is done

--- loan/router.go
+++ loan/router.go
@@ -1,6 +1,8 @@
 package loan
 
 import (
+	"github.com/ArminGodiz/Gochain/logger"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -91,4 +93,27 @@ var routes = Routes{
 		"/user/{playerName}",
 		controller.GetLoansForUser,
 	},
+}
+
+//NewRouter configures a new router to the API
+func NewRouter(nodeAddress string) *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	controller.currentNodeURL = "http://localhost:" + nodeAddress
+
+	// create Genesis block
+	controller.blockchain.CreateNewBlock(100, "0", "0")
+
+	for _, route := range routes {
+		var handler http.Handler
+		handler = route.HandlerFunc
+		handler = logger.Logger(handler, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+
+	}
+	return router
 }
--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"github.com/ArminGodiz/Gochain/loan"
+	"github.com/gorilla/handlers"
 	"log"
 	"net/http"
 	"os"
-	"github.com/gorilla/handlers"
 )
 
 func main() {
 	port := os.Args[1] // inputted port
-	//router := bet.NewRouter(address)
+	router := loan.NewRouter(port)
 
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST"})
-
 	// launch server
 	log.Fatal(http.ListenAndServe(":"+port,
 		handlers.CORS(allowedOrigins, allowedMethods)(router)))
