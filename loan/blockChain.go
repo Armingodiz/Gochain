a2f897db907203df51685800900ecd755e79dfd9
ArminGodiz <armingodarzi1380@gmail.com>
2021-03-24 22:41:52 +0430

    RegisterNode added to blockchain

--- loan/blockChain.go
+++ loan/blockChain.go
@@ -10,4 +10,22 @@ func (b *Blockchain) RegisterBet(loan Loan) bool {
 	return true
 }
 
+//RegisterNode registers a node in our blockchain
+func (b *Blockchain) RegisterNode(node string) bool {
+	if !contains(b.NetworkNodes, node) {
+		b.NetworkNodes = append(b.NetworkNodes, node)
+	}
+	return true
+}
+
+// function to check if new node is already added to nodes
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
 
