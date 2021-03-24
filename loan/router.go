25b835556ff6c39ada854491925f636c61d9948f
ArminGodiz <armingodarzi1380@gmail.com>
2021-03-24 22:29:02 +0430

    Route struct in router added

--- loan/router.go
+++ loan/router.go
@@ -1,1 +1,19 @@
 package loan
+
+import (
+	"net/http"
+)
+
+var controller = &Controller{
+	blockchain: &Blockchain{
+		Chain:        Blocks{},
+		PendingLoans: Loans{},
+		NetworkNodes: []string{}}}
+
+// Route defines a route
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
